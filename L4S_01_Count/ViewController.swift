924411e6b760b79ce33fa8adaafa6d602fb4710c
yukiyuki961 <yukinabe3561@icloud.com>
2019-02-11 16:40:28 +0900

    first commit

--- L4S_01_Count/ViewController.swift
+++ L4S_01_Count/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+    }
 
 }
 
